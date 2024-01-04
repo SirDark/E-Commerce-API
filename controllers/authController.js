29fb43cc70d1cd72005999c6a9174ada4870663c
SirDark <gergelykevin123@gmail.com>
2024-01-04 22:11:02 +0100

    changed logout cookie expiration

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ const register = async (req,res) => {
 const logout = async (req,res) => {
     res.cookie('token', 'logout',{
         httpOnly:true,
-        expires:new Date(Date.now() + 5*1000)
+        expires:new Date(Date.now())
     })
     res.status(StatusCodes.OK).json({msg: 'user logged out'})
 }
