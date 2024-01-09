0fe6c133522d329cf6f52cecc254d862d37d8a2c
SirDark <gergelykevin123@gmail.com>
2024-01-09 16:15:50 +0100

    changes in the schema

--- models/review.js
+++ models/review.js
@@ -3,23 +3,33 @@ const { default: mongoose } = require("mongoose");
 const reviewSchema = new mongoose.Schema({
     rating:{
         type:Number,
+        min:1,
+        max:5,
         required:[true, 'please give a rating']
     },
     title:{
         type:String,
-        default: ""
+        trim:true,
+        required:[true, 'please provide a title'],
+        maxLength: 100
     },
     comment:{
         type:String,
-        default: ""
+        required:[true, 'please provide a comment'],
+        maxLength: 1000
     },
     user:{
         type: mongoose.Types.ObjectId,
+        ref:'User',
+        required:true
     },
     product:{
         type: mongoose.Types.ObjectId,
+        ref:'Product',
         required:true
     }
 }, {timestamps:true})
+reviewSchema.index({product:1, user:1}, {unique:true})
+
 
 models.exports = mongoose.model('Review', reviewSchema)
